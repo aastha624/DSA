69d5970d9e06ed03a5ca6ad47e5b354d41962bf5
aastha624 <patelaastha2910@gmail.com>
2024-03-12 23:19:16 +0530

    traversal

---
+++ traversal.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void display(int arr[],int n);
+void display(int arr[],int n){
+    //travesal
+    for( int i=0;i<n;i++){
+        printf("%d", arr[i] );
+    }
+    printf("\n");
+
+}
+
+int main(){
+    int arr[100]={1,7,67,78};
+    display(arr , 4);
+    return 0;
+}
